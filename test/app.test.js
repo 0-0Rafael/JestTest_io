869bd4c7acfc8c0fad5001dd4168c404052d2a27
Rafael Ariel Custodio Guillen <107286551+0-0Rafael@users.noreply.github.com>
2023-11-21 17:33:23 -0400

    All structure of the proyecto Jest

---
+++ test/app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Pruebas Unitarias', () => {
+  it('debería pasar esta prueba siempre', () => {
+    expect(true).toBe(true);
+  });
+
+  it('debería contener el mensaje "¡Hola, mundo!"', async () => {
+    const response = await request(app).get('/');
+    // Verificar que el HTML contiene la cadena esperada
+    expect(response.text).toContain('Welcome to Express');
+  });
+});
