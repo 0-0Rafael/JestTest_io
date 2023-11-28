33eed51d3a9a0fab128687feb65fec66eb6ebf5f
Rafael Ariel Custodio Guillen <107286551+0-0Rafael@users.noreply.github.com>
2023-11-28 16:20:55 -0400

    prueba good

--- test/app.test.js
+++ test/app.test.js
@@ -3,13 +3,12 @@ const app = require('../app');
 
 
 describe('Pruebas Unitarias', () => {
-  
+
   it('debería contener el mensaje "Welcome to Express"', async () => {
     const response = await request(app).get('/');
-    // Verificar que el HTML contiene la cadena esperada
+    
     expect(response.text).toContain('Welcome to Express');
   });
-  
 
   
 });
