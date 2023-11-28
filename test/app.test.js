3dee25e30e7722bec1946b6c0bafe6e2ee1a97af
Rafael Ariel Custodio Guillen <107286551+0-0Rafael@users.noreply.github.com>
2023-11-28 16:10:13 -0400

    prueba que si funciona

--- test/app.test.js
+++ test/app.test.js
@@ -2,25 +2,14 @@ const request = require('supertest');
 const app = require('../app');
 
 
-// Cambia el contenido esperado para que la prueba falle
-it('debería contener el mensaje "¡Hola, mundo!"', async () => {
-  const response = await request(app).get('/');
-  // Cambia la cadena esperada a algo que no esté en la respuesta real
-  expect(response.text).toContain('Mensaje Inesperado');
-});
-
-
-/*
 describe('Pruebas Unitarias', () => {
-  it('debería pasar esta prueba siempre', () => {
-    expect(true).toBe(true);
-  });
-
-  it('debería contener el mensaje "¡Hola, mundo!"', async () => {
+  
+  it('debería contener el mensaje "Welcome to Express"', async () => {
     const response = await request(app).get('/');
     // Verificar que el HTML contiene la cadena esperada
     expect(response.text).toContain('Welcome to Express');
   });
-});
-*/
+  
 
+  
+});
