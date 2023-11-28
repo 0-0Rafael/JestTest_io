641d3dcac38e68a590ec04e9487d4ac89d2f749d
Rafael Ariel Custodio Guillen <107286551+0-0Rafael@users.noreply.github.com>
2023-11-28 15:36:47 -0400

    prueba jest prueba jest

--- test/app.test.js
+++ test/app.test.js
@@ -1,16 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
 
-
-// Cambia el contenido esperado para que la prueba falle
-it('debería contener el mensaje "¡Hola, mundo!"', async () => {
-  const response = await request(app).get('/');
-  // Cambia la cadena esperada a algo que no esté en la respuesta real
-  expect(response.text).toContain('Mensaje Inesperado');
-});
-
-
-/*
 describe('Pruebas Unitarias', () => {
   it('debería pasar esta prueba siempre', () => {
     expect(true).toBe(true);
@@ -22,4 +12,3 @@ describe('Pruebas Unitarias', () => {
     expect(response.text).toContain('Welcome to Express');
   });
 });
-*/
